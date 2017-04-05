4b901e9c58a75debe868d4ca3fad9c83f17f2ff5
IMANE RAHIL <imanerahil2017@gmail.com>
2017-04-05 09:24:00 +0100

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+
+
+{
+    'name': 'Project - Agil',
+    'category': "Project",
+    'summary': 'chiffer votre projet .',
+    'website': 'https://www.agilorg.com/',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': """
+        
+        """,
+    'author': 'imane',
+    'depends': ['project'],
+    'data': [
+        'views/project_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'qweb': [],
+    'images': [
+        'static/description/logo.png',
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+    'application': True,
+}
